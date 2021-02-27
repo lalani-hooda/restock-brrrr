1731848fc2f559b93f8184ccca62a66f22176d7f
Zeeshan Hooda <zhooda@protonmail.com>
2021-02-26 21:06:45 -0700

    added: flask server boilerplate & demo routes

--- src/main.py
+++ src/main.py
@@ -1,1 +1,22 @@
-print('hello world!')
+import flask
+
+# Create a flask app
+app = flask.Flask(__name__)
+
+# Root route for app (example.com/)
+@app.route("/")
+def index():
+    return "<h1>hello world!</h1><br><h3>use route /hi/<username> to recieve a greeting</h3>"
+
+# Hi route without valid input
+@app.route("/hi/")
+def who():
+    return "<h1>who are you?</h1>"
+
+# Hi route with valid input
+@app.route("/hi/<username>")
+def greet(username):
+    return f"<h1>hello, {username}!<h1>"
+
+# Development server only, not for production
+app.run()
